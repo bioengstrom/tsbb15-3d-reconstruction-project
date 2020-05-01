6d6e6a812d66f297ca1efb34f077704beab7ec29
Ylva Selling <ylva.selling@gmail.com>
2020-05-01 11:28:03 +0200

    Naive implementation of normals work

--- 3Dvisualization.py
+++ 3Dvisualization.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
 
 #Get point cloud data (PCD)
 points = np.genfromtxt("stanfordbunny.txt", delimiter=" ")
@@ -11,22 +13,46 @@ tree = KDTree(points)
 
 #Look for the n nearest neighbours points in a sphere with r radius
 r = 0.01
-index = tree.query_radius(points[0:1], r)  # NNs within distance of r of point
+NN_index = tree.query_radius(points, r)  # NNs within distance of r of point
+normals = np.zeros(points.shape)
 
-# Plot selected points
-pi = points[index[0]]
-not_selected_points = np.delete(points, index[0], axis=0)
-plt.figure()
-ax = plt.subplot(111, projection='3d')
-ax.scatter(not_selected_points[:,0], not_selected_points[:,1], not_selected_points[:,2], c='b')
-ax.scatter(pi[:,0], pi[:,1], pi[:,2], c='r')
+for i in range(NN_index.shape[0]):
+    #Get the k points that are in the radius
+    pi = points[NN_index[i]]
+
+    #Compute normal from points with help of eigenvalues
+    k = pi.shape[0]
+    pbar = (1/k)*np.sum(pi, axis=0) #TODO: Check so that outer product makes sense and compute M
+    pi_piT = pi[:,:,None]*pi[:, None, :]
+    pbar_pbarT = pbar*pbar[None, :,None]
+    M = (1/k)*np.sum(pi_piT-pbar_pbarT,axis=0) #Pbar is ok
+
+    #Compute eigenvalues of M
+    eigenvalues, eigenvectors = np.linalg.eig(M)
+
+    #Find smallest eigenvalue: this corresponds to the eigenvector that is the surface normal
+    normal_NN_index = np.argmin(eigenvalues)
+    normal = eigenvectors[normal_NN_index]
+    normals[i] = normal
+
+# Plot vector and selected k points for last point
+last_i = NN_index.shape[0]-1
+pi = points[NN_index[last_i]]
+not_selected_points = np.delete(points, NN_index[last_i], axis=0)
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(not_selected_points[:,0], not_selected_points[:,1], not_selected_points[:,2], 'o', markersize=10, color='b', alpha=0.2)
+ax.plot(pi[:,0], pi[:,1], pi[:,2], 'o', markersize=10, color='r', alpha=0.2)
+
+#Plot vector for normal
+ax.quiver(
+        points[last_i,0], points[last_i,1], points[last_i,2], # <-- starting point of vector
+        normals[last_i,0], normals[last_i,1], normals[last_i,2], # <-- directions of vector
+        arrow_length_ratio=0.001, color = 'red', alpha = 0.3, lw = 1
+    )
 plt.show()
 
-#Compute normal from points with help of eigen values
-k = pi.shape[0]
-p_bar = (1/k)*np.sum(pi, axis=0) #TODO: Check so that outer product makes sense and compute M
-print(pi[:2])
-print(pi[:2]*pi[:2,None])
-
-M = (1/k)*np.sum((pi*pi[:,None])-(p_bar*p_bar[:,None]),axis=0)
-print(M.shape)
+#Print points and normals to file
+result = np.append(points, normals, axis=1)
+np.savetxt("bunny_normals.txt", result, delimiter=' ')
+print("Printed results to file")
