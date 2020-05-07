5afacc07fce2c7816d44cfcfa3c8571d9ddfca96
asd <asdasd>
2020-05-07 14:29:24 +0200

    pickled F

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import fun
+import scipy.io as sio
+import numpy as np
 import cv2 as cv
+import pickle
 
 print("Hello World")
 
@@ -8,7 +11,9 @@ img2 = cv.imread("images/viff.001.ppm", cv.IMREAD_COLOR)
 img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
 img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
-F, *_ = fun.f_matrix(img1, img2)
+#F, *_ = fun.f_matrix(img1, img2)
+
+F = np.load('F_matrix.npy')
 
 Dino_36C = sio.loadmat('imgdata\dino_Ps.mat')
 
@@ -21,7 +26,7 @@ R = np.zeros((C.shape[1],3,3))
 t = np.zeros((C.shape[1],3))
 
 for i in range(C.shape[1]):
-    K[i,:,:], R[i,:,:], t[i,:] = camera_resectioning(C[0,i,:,:])
+    K[i,:,:], R[i,:,:], t[i,:] = fun.camera_resectioning(C[0,i,:,:])
 #print(K)
 #print(R)
 #print(t)
