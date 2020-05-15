Fixed K and jacobian sparsity mask. Working on addNewPoints

correspondences.py
@@ -7,7 +7,7 @@ class Correspondences:
 
     def getCorrByIndices(self,i1, i2):
         y1 = self.points[:,i1*2:(i1*2)+2]
-        y2 = self.points[:,i1*2:(i1*2)+2]
+        y2 = self.points[:,i2*2:(i2*2)+2]
 
         #Remove all rows in y1 and y2 that has -1:s in any of y1 and y2
         is_correspondence_y1 = np.array([np.any(y1 != -1, axis=1)], dtype='bool')
@@ -17,3 +17,16 @@ class Correspondences:
         y2 = np.array(y2[correspondence[0], :])
 
         return y1, y2
+"""
+    def getNoOfCorrespondences(self, i1, i2):
+        size = i2-i1
+        correspondence = np.zeros([size, points.shape[0]], dtype='bool')
+        for i in range(i1, 1, i2):
+            y1 = self.points[:,i1*2:(i1*2)+2]
+            y2 = self.points[:,i1*2:(i1*2)+2]
+            #Remove all rows in y1 and y2 that has -1:s in any of y1 and y2
+            correspondence = np.array([np.any(y1 != -1, axis=1)], dtype='bool')
+            is_correspondence_y2 = np.array([np.any(y2 != -1, axis=1)], dtype='bool')
+            corr_y1_y2 = np.logical_and(is_correspondence_y1, is_correspondence_y2)
+            correspondence = np.logical_and(corr_y1_y2, is_correspondence_y2)
+"""

fun.py
@@ -8,6 +8,19 @@ import math
 import scipy.io as sio
 from correspondences import Correspondences
 
+def decomposeP(P):
+
+    M = P[0:3,0:3]
+    Q = np.eye(3)[::-1]
+    P_b = Q @ M @ M.T @ Q
+    K_h = Q @ np.linalg.cholesky(P_b) @ Q
+    K = K_h / K_h[2,2]
+    A = np.linalg.inv(K) @ M
+    l = (1.0/np.linalg.det(A)) ** (0.33333333)
+    R = l * A
+    t = l * np.linalg.inv(K) @ P[0:3,3]
+    return K, R, t
+
 def MakeHomogenous(K, coord):
     #Normalizing corresponding points
     coord = coord.T
@@ -156,8 +169,8 @@ def specRQ(M):
     #maybe right implementation of special rq
     U, Q = scipy.linalg.rq(M)
     if np.linalg.det(Q) == -1:
-        U[0,:] = U[0,:]*-1
-        Q[0,:] = Q[0,:]*-1
+        U[0,:] = U[0,:]*-1.0
+        Q[:,0] = Q[:,0]*-1.0
 
     return U, Q
 

main.py
@@ -25,7 +25,7 @@ C = fun.getCameraMatrices()
 #Get putative correspondence points
 correspondences = fun.Correspondences()
 y1, y2 = correspondences.getCorrByIndices(0,1)
-#y2, y3 = correspondences.getCorrByIndices(0,2)
+#y2, y3 = correspondences.getNoOfCorrespondences(0,2)
 #print(y2.shape())
 
 """
@@ -56,19 +56,27 @@ print("Initialize tables...")
 T_tables = Tables()
 
 E, K = fun.getEAndK(C, F)
+print("Vår K")
+print(K)
+
+K, R, t = fun.decomposeP(C[0,0,:,:])
+print("Inte vår K")
+print(K)
+
+E = np.matmul(np.transpose(K),np.matmul(F,K))
 
 #Get R and t from E
 R, t = fun.relative_camera_pose(E, y1[0], y2[0])
-result = cv.decomposeProjectionMatrix(C[0,0,:,:])
-result = np.array(result)
-K = result[0]
-R = result[1]
-t = result[2]
-t = t[:3,0]
+"""
+print(K.shape)
+print(R.shape)
+print(t.shape)
+"""
 
 #Make the image coordinates homogenous
 y1 = fun.MakeHomogenous(K, y1p)
 y2 = fun.MakeHomogenous(K, y2p)
+#print(y1)
 
 #Get first two camera poses
 C1 = CameraPose()
@@ -103,8 +111,7 @@ T_tables.plot()
 """
 
 #for i in range(images.shape[0]-1):
-for i in range(1,2,1):
-
+for i in range(1,5,1):
     #Select inlier 3D points T'points
     """
         BA: Bundle Adjustment of all images so far
@@ -125,6 +132,7 @@ for i in range(1,2,1):
     """
 
     yp2, yp3 = correspondences.getCorrByIndices(i,i+1)
+    #print(yp2.shape)
     lab3.show_corresp(images[i], images[i+1], yp2.T, yp3.T)
     plt.show()
     yp2_hom = fun.MakeHomogenous(K, yp2)
@@ -135,20 +143,18 @@ for i in range(1,2,1):
         EXT2: Find 2D<->3D correspondences. Algorithm 21.2
         EXT3: PnP -> R,t of new view and consensus set C
     """
-    """
-    A_y1, A_y2 = T_tables.addNewView(K, images[1], images[2], 2, yp2_hom[:100], yp3_hom[:100], yp2[:100], yp3[:100])
+
+    A_y1, A_y2 = T_tables.addNewView(K, images[1], images[2], 2, yp2_hom, yp3_hom, yp2, yp3)
     T_tables.plot()
-    mask = T_tables.sparsity_mask()
-    plt.imshow(mask)
     plt.show()
-    """
+
 
     """
         EXT4: Extend table with new row and insert image points in C. Algorithm 21.3
         EXT5: For each putative correspondence that satisfies E, extend table with column
     """
     #Add new 3D points
-    #T_tables.addNewPoints(A_y1, A_y2, i, i+1)
+    T_tables.addNewPoints(A_y1, A_y2, i, i+1)
 
 
 

tables.py
@@ -110,6 +110,8 @@ class Tables:
             #Got through all observations seen in last view added
             for v in self.T_views[len(self.T_views)-1].observations_index:
                 o = self.T_obs[v]
+                #print(o.image_coordinates)
+                #print(y1_hom[i])
                 if np.linalg.norm(o.image_coordinates-y1_hom[i]) < 0.001:
                     #There is a corresponding 3D point x in T_points! Add y2, x to D
                     j = o.point_3D_index
@@ -152,7 +154,10 @@ class Tables:
         C1 = self.T_views[view_index_1].camera_pose
         C2 = self.T_views[view_index_2].camera_pose
 
+        R = C2.R @ C1.R.T
+        t = C2.t - C2.R @ C1.R.T @ C1.t
 
+        E = R.T
         return A_y1
 
     def plot(self):
@@ -234,21 +239,19 @@ class Tables:
 
     #function that computes a suitable sparsity pattern as a function of the number of point correspondences
     def sparsity_mask(self):
-        #n_C = The_table.T_views.shape[0]
-        #n_P = The_table.T_points.shape[0]
-
-        #m = (self.T_views)*2
-        #n =
 
         Jc = np.zeros((len(self.T_obs)*2, (len(self.T_views)*12)))
         Jp = np.zeros((len(self.T_obs)*2, (len(self.T_points)*3)))
-
+        k = 0
         for i,o in enumerate (self.T_obs):
-            Jc[i*2,o.view_index*12:(o.view_index*12)+12] = 1
-            Jp[(i*2)+1,o.point_3D_index*3:(o.point_3D_index*3)+3] = 1
+            if(o.view_index == 0):
+                continue
+            Jc[k:k+1,o.view_index*12:(o.view_index*12)+12] = 1
+            Jp[k:k+1,o.point_3D_index*3:(o.point_3D_index*3)+3] = 1
+            k = k+2
 
         J_mask = np.hstack((Jc,Jp))
-        #print(J_mask.shape)
+
         return J_mask
 
     def updateCameras3Dpoints2(self, new_pose, new_points):