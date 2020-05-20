3e16cbeabdcaae43f363290de244674adbeda3be
Ylva Selling <ylva.selling@gmail.com>
2020-05-20 10:41:56 +0200

    Changed file pathway

--- fun.py
+++ fun.py
@@ -56,7 +56,7 @@ def MakeHomogenous(K, coord):
 
 def getImages():
     no_of_images = 36
-    img1 = cv.imread("images/viff.000.ppm", cv.IMREAD_COLOR)
+    img1 = cv.imread("../images/viff.000.ppm", cv.IMREAD_COLOR)
     img1 = np.asarray(img1)
 
 
@@ -68,7 +68,7 @@ def getImages():
             no = '0' + no
         #img1 = np.asarray(cv.cvtColor(images[0], cv.COLOR_BGR2GRAY)) # Grayscale
         #img2 = np.asarray(cv.cvtColor(images[1], cv.COLOR_BGR2GRAY))
-        images[i] = np.asarray(cv.imread("images/viff.0" + no + ".ppm", cv.IMREAD_COLOR))
+        images[i] = np.asarray(cv.imread("../images/viff.0" + no + ".ppm", cv.IMREAD_COLOR))
     return images
 
 def getCameraMatrices():
